a0dc42fd8d0b40284cfe1c7cd09241fd43ca886d
lorenzo456 <lorenzo456_@hotmail.com>
2025-09-13 15:27:54 +0200

    Added windows support (possibly broke linux)

---
+++ build/CMakeFiles/MyGame.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Development/Games/MegamanClone/bullet.cpp" "CMakeFiles/MyGame.dir/bullet.cpp.obj" "gcc" "CMakeFiles/MyGame.dir/bullet.cpp.obj.d"
+  "C:/Development/Games/MegamanClone/enemy.cpp" "CMakeFiles/MyGame.dir/enemy.cpp.obj" "gcc" "CMakeFiles/MyGame.dir/enemy.cpp.obj.d"
+  "C:/Development/Games/MegamanClone/main.cpp" "CMakeFiles/MyGame.dir/main.cpp.obj" "gcc" "CMakeFiles/MyGame.dir/main.cpp.obj.d"
+  "C:/Development/Games/MegamanClone/obstacle.cpp" "CMakeFiles/MyGame.dir/obstacle.cpp.obj" "gcc" "CMakeFiles/MyGame.dir/obstacle.cpp.obj.d"
+  "C:/Development/Games/MegamanClone/player.cpp" "CMakeFiles/MyGame.dir/player.cpp.obj" "gcc" "CMakeFiles/MyGame.dir/player.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MyGame.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyGame.dir/bullet.cpp.obj"
+  "CMakeFiles/MyGame.dir/bullet.cpp.obj.d"
+  "CMakeFiles/MyGame.dir/enemy.cpp.obj"
+  "CMakeFiles/MyGame.dir/enemy.cpp.obj.d"
+  "CMakeFiles/MyGame.dir/main.cpp.obj"
+  "CMakeFiles/MyGame.dir/main.cpp.obj.d"
+  "CMakeFiles/MyGame.dir/obstacle.cpp.obj"
+  "CMakeFiles/MyGame.dir/obstacle.cpp.obj.d"
+  "CMakeFiles/MyGame.dir/player.cpp.obj"
+  "CMakeFiles/MyGame.dir/player.cpp.obj.d"
+  "MyGame.exe"
+  "MyGame.exe.manifest"
+  "MyGame.pdb"
+  "libMyGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
